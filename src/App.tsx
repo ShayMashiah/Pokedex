3449e1856813243d916cf0e8a8484d5e28723df3
ShayMashiah <shay2908@gmail.com>
2025-05-25 17:18:34 +0300

    feat: creation of generic button component and styling acorrding to type and size

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { Button } from "../src/components/ui/button"
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
+      <Button variant="primary" disabled>Click me</Button>
+      <Button variant="primary" size="md" disabled>Click me</Button>
+      <Button variant="primary" size="lg" disabled>Click me</Button>
     </div>
   )
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,8 +1,8 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
@@ -11,6 +11,8 @@ const buttonVariants = cva(
       variant: {
         default:
           "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
+        primary:
+          "bg-[#3B5AA6] text-white hover:bg-[#324c8b] active:bg-[#182442] active:shadow-[inset_4px_4px_13px_0px_#0000000D] disabled:bg-[#E3E6E9] disabled:text-gray-500 disabled:cursor-not-allowed aria-disabled:bg-[#E3E6E9] aria-disabled:text-gray-500",
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
@@ -23,8 +25,9 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        sm: "w-[76px] h-[32px] pt-[9px] pb-[9px] pr-[16px] pl-[16px] gap-[4px] rounded-[4px]",
+        md: "w-[82px] h-[36px] pt-[10px] pb-[10px] pr-[16px] pl-[16px] gap-[4px] rounded-[4px]",
+        lg: "w-[82px] h-[40px] pt-[12px] pb-[12px] pr-[16px] pl-[16px] gap-[4px] rounded-[4px]",
         icon: "size-9",
       },
     },
@@ -33,7 +36,7 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 function Button({
   className,
@@ -43,9 +46,9 @@ function Button({
   ...props
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
+    asChild?: boolean;
   }) {
-  const Comp = asChild ? Slot : "button"
+  const Comp = asChild ? Slot : "button";
 
   return (
     <Comp
@@ -53,7 +56,7 @@ function Button({
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
     />
-  )
+  );
 }
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
