c86e784acdfd3bf9d12b25b12386b5ec2bc43f45
ShayMashiah <shay2908@gmail.com>
2025-05-25 15:02:46 +0300

    feat: shudcn configuration and initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css'
+import { Button } from "../src/components/ui/button"
 
 function App() {
-
   return (
-    <>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
   )
 }
 
