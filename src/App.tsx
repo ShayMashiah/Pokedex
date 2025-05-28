7663f73b7cf9546a18057ec0a7314eacb5095d7d
ShayMashiah <shay2908@gmail.com>
2025-05-28 10:18:45 +0300

    fix: remove ^ from dep, update progress bar colors, remove unnecesary code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,4 @@
-import { useState, useEffect } from "react";
-import { Progress } from "./components/ui/progress";
-
 function App() {
-  const [progress, setProgress] = useState(100);
-
-  console.log("Progress value:", progress);
-
-  useEffect(() => {
-    const timer = setTimeout(() => setProgress(10), 500);
-    console.log(progress);
-    return () => clearTimeout(timer);
-  }, []);
-
-  return <Progress value={progress} className="w-[60%]" />;
 }
 
 export default App;
--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -12,17 +12,17 @@ function Progress({
 }) {
     const indicatorColor =
     value < 33
-      ? "bg-red-500"
+      ? "bg-extendedPalette-error-red"
       : value < 66
-      ? "bg-yellow-400"
-      : "bg-green-400";
+      ? "bg-extendedPalette-warning-yellow"
+      : "bg-extendedPalette-success-green";
 
   return (
     <ProgressPrimitive.Root
       data-slot="progress"
       value={value}
       className={cn(
-        "bg-neutrals-400 relative h-4 w-full overflow-hidden rounded-lg border border-neutrals-black",
+        "bg-neutrals-400 relative h-4 overflow-hidden rounded-lg border border-neutrals-black",
         "w-270 h-14 rounded-xs border top-20 left-20",
         className
       )}
