bc3b1b3be1c4293fc8b4c700dfc496b64a6dd925
ShayMashiah <shay2908@gmail.com>
2025-06-15 18:45:43 +0300

    feat: add design vector in pre-battle page

--- src/pages/PreBattlePage.tsx
+++ src/pages/PreBattlePage.tsx
@@ -7,7 +7,7 @@ import rightBackground from "@/assets/bluebg.jpg";
 import leftBackground from "@/assets/purplebg.jpg";
 
 function PreBattlePage() {
-  const [rivalPokemon, setRivalPokemon] = useState<Pokemon | null >(null);
+  const [rivalPokemon, setRivalPokemon] = useState<Pokemon | null>(null);
   const navigate = useNavigate();
 
   const location = useLocation();
@@ -50,6 +50,38 @@ function PreBattlePage() {
         </div>
         <div className="relative flex justify-center items-center w-full mt-24">
           <div className="relative flex h-755 overflow-hidden">
+            <div className="absolute top-0 right-0 w-full h-full pointer-events-none z-20">
+              <svg
+                width="100%"
+                height="100%"
+                viewBox="0 0 1363 754"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="block"
+              >
+                <polyline
+                  points="
+                      1363,0
+                      1250,150
+                      1050,180
+                      850,350
+                      750,400
+                      650,400
+                      550,400
+                      450,500
+                      350,500
+                      250,500
+                      0,754
+                      0,0"
+                  stroke="white"
+                  strokeWidth="11"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  filter="drop-shadow(0 4px 4px rgba(0,0,0,0.25))"
+                />
+              </svg>
+            </div>
+
             <div className="relative w-1/2 h-full">
               <img
                 src={leftBackground}
@@ -80,15 +112,15 @@ function PreBattlePage() {
               )}
             </div>
 
-            <div className="absolute w-75 h-100 inset-0 flex items-center justify-center mr-55">
+            <div className="absolute w-75 h-100 inset-0 flex items-center justify-center leading-[48px] mr-55 z-20">
               <span
-                className="text-[130px] font-sans font-extrabold text-secondary-300 drop-shadow-[0_2px_4px_rgba(0,0,0,0.5)]"
+                className="text-[130px] font-sans font-extrabold  text-secondary-300 drop-shadow-[0_2px_4px_rgba(0,0,0,0.5)]"
                 style={{ WebkitTextStroke: "3px white" }}
               >
                 V
               </span>
             </div>
-            <div className="absolute w-75 h-100 inset-0 flex items-center justify-center ml-55 mt-72">
+            <div className="absolute w-75 h-100 inset-0 flex items-center justify-center leading-[48px] ml-55 mt-72 z-20">
               <span
                 className="text-[130px] font-sans font-extrabold text-secondary-300 drop-shadow-[0_2px_4px_rgba(0,0,0,0.5)]"
                 style={{ WebkitTextStroke: "3px white" }}
