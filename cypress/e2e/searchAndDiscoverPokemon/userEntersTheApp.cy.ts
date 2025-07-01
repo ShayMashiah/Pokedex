8bd60f4f04ce6bdcd955f175227de67807683aea
ShayMashiah <shay2908@gmail.com>
2025-07-01 14:56:47 +0300

    fix: use '[data-cy=pokemon-table]' instead of id.

--- cypress/e2e/searchAndDiscoverPokemon/userEntersTheApp.cy.ts
+++ cypress/e2e/searchAndDiscoverPokemon/userEntersTheApp.cy.ts
@@ -2,11 +2,11 @@ describe("User enters the app", () => {
   it("displays the Pokémon table with full data", () => {
     cy.visit("/");
 
-    cy.get("#pokemon-table", { timeout: 10000 }).should("exist");
+    cy.get('[data-cy="pokemon-table"]', { timeout: 10000 }).should("exist");
 
-    cy.get("#pokemon-table tbody tr").should("have.length.greaterThan", 0);
+    cy.get('[data-cy="pokemon-table"] tbody tr').should("have.length.greaterThan", 0);
 
-    cy.get("#pokemon-table").within(() => {
+    cy.get('[data-cy="pokemon-table"]').within(() => {
       cy.contains("Pokemon name");
       cy.contains("ID");
       cy.contains("Description");
