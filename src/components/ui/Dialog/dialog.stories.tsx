aaa7ef63ef58913f1162938162eb2865a7b17496
ShayMashiah <shay2908@gmail.com>
2025-07-06 15:23:04 +0300

    fix: use mock data instead of backend data for storybook.

--- src/components/ui/Dialog/dialog.stories.tsx
+++ src/components/ui/Dialog/dialog.stories.tsx
@@ -2,9 +2,9 @@ import { Dialog, DialogTrigger, DialogContent } from "./dialog";
 import { Variant } from "../../../lib/constants";
 import { Button } from "../Button/button";
 import type { Meta, StoryObj } from "@storybook/react";
-import { usePokemonById } from "../../../lib/hooks/usePokemonById";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MemoryRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { mockPokemon } from "../../../lib/mockData";
 
 const queryClient = new QueryClient();
 
@@ -27,16 +27,12 @@ export default meta;
 type Story = StoryObj<typeof Dialog>;
 
 const PokemonInfoComponent = () => {
-  const { data: pokemon } = usePokemonById(1);
-
-  if (!pokemon) return null;
-
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Button variant="primary" size="xlg">Show Pokemon Info</Button>
       </DialogTrigger>
-      <DialogContent variant={Variant.PokeInfo} pokemon={pokemon} />
+      <DialogContent variant={Variant.PokeInfo} pokemon={mockPokemon} />
     </Dialog>
   );
 };
--- src/components/ui/Table/table.stories.tsx
+++ src/components/ui/Table/table.stories.tsx
@@ -1,48 +1,38 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import PokemonTable from "./PokemonTable";
-import { useAllPokemons } from "../../../lib/hooks/useAllPokemons";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { mockTableData } from "@/lib/mockData";
+
+const queryClient = new QueryClient();
 
 const meta: Meta<typeof PokemonTable> = {
   title: "UI/Table",
   component: PokemonTable,
   tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <QueryClientProvider client={queryClient}>
+        <Story />
+      </QueryClientProvider>
+    ),
+  ],
 };
 
 export default meta;
 type Story = StoryObj<typeof PokemonTable>;
 
-const queryClient = new QueryClient();
 
 export const Default: Story = {
-  render: () => {
-    return (
-      <QueryClientProvider client={queryClient}>
-        <PokemonTableWithData />
-      </QueryClientProvider>
-    );
-  },
-};
-
-const PokemonTableWithData = () => {
-  const { data } = useAllPokemons(1, 10);
-
-  if (!data) return null;
-
-
-  console.log("data", data);
-
-
-  return (
+  render: () => (
     <PokemonTable
-      data={data.data}
-      totalCount={data.totalCount}
-      totalPages={data.totalPages}
+      data={mockTableData }
+      totalCount={3}
+      totalPages={1}
       currentPage={1}
       onPageChange={() => {}}
       onPageSizeChange={() => {}}
       itemsPerPage={10}
       activeTab="all"
     />
-  );
+  ),
 };
